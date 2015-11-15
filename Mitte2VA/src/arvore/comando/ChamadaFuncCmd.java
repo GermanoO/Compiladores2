faef0be2406644e57009fbda66ea63df3fd06396
GermanoO <bgermano89@gmail.com>
2015-11-15 02:14:58 -0300

    ChamadaFuncCmd nova

--- Mitte2VA/src/arvore/comando/ChamadaFuncCmd.java
+++ Mitte2VA/src/arvore/comando/ChamadaFuncCmd.java
@@ -2,12 +2,29 @@ package arvore.comando;
 
 import java.util.List;
 
+import arvore.expressao.ChamadaFunc;
 import arvore.expressao.Expressao;
 
 public class ChamadaFuncCmd implements Comando {
 	private String nomeFuncao;
 	private List<Expressao> listaExprs;
+	
+	//alvez seja assim
+	private ChamadaFunc chamadaFuncao;
 
+	//Construtor Antigo
+	public ChamadaFuncCmd(String nomeFuncao, List<Expressao> listaExprs) {
+		super();
+		this.nomeFuncao = nomeFuncao;
+		this.listaExprs = listaExprs;
+	}
 
+	//Construtor Novo
+	public ChamadaFuncCmd(ChamadaFunc chamadaFuncao) {
+		super();
+		this.chamadaFuncao = chamadaFuncao;
+	}
+
+	
 	
 }
--- Mitte2VA/src/arvore/expressao/ChamadaFunc.java
+++ Mitte2VA/src/arvore/expressao/ChamadaFunc.java
@@ -5,6 +5,13 @@ import java.util.List;
 public class ChamadaFunc implements Expressao {
 	private String nomeFuncao;
 	private List<Expressao> listaExprs;
+	
+	
+	public ChamadaFunc(String nomeFuncao, List<Expressao> listaExprs) {
+		super();
+		this.nomeFuncao = nomeFuncao;
+		this.listaExprs = listaExprs;
+	}
 
 	
 }
